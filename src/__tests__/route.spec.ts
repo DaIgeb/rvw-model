2b6408a110430c046931bf303c20537a074c1cb3
Daniel Roth <drot@vzch.com>
2019-11-10 21:23:32 +0100

    Extending the test

--- src/__tests__/route.spec.ts
+++ src/__tests__/route.spec.ts
@@ -1,5 +1,12 @@
 import { validate } from '../route';
 
-test('validate', () => {
-    expect(validate({ id: 'Carl' })).toBe(false);
+test('validate', done => {
+    expect(validate({ id: 'Carl' }, {
+        error: (args) => {
+            expect(args.length).toBe(1);
+            expect(args[0].schemaPath).toBe('#/properties/id/format');
+            expect(args[0].message).toBe('should match format "uuid"');
+            done();
+        }
+    })).toBe(false);
 });
